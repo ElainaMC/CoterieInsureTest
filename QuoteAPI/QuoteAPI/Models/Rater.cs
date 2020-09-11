4dcb6fd6549ecd60aaa03b5a6e3117ac17ccf947
ElainaMC <elainam82@gmail.com>
2020-09-11 15:09:03 -0700

    Rater model

---
+++ QuoteAPI/QuoteAPI/Models/Rater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class Rater
+    {
+        public int Revenue { get; set; }
+
+        public string State { get; set; }
+
+        public string Business { get; set; }
+    }
+}
