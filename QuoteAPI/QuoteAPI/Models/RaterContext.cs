fb146c0df6aa51df5c19e88eb63d895ffe3af8f8
ElainaMC <elainam82@gmail.com>
2020-09-11 16:02:09 -0700

    Connection String

---
+++ QuoteAPI/QuoteAPI/Models/RaterContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class RaterContext: DbContext
+    {
+        public RaterContext(DbContextOptions<RaterContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Rater>Rater { get; set; }
+    }
+}
--- QuoteAPI/QuoteAPI/Startup.cs
+++ QuoteAPI/QuoteAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using QuoteAPI.Models;
 
 namespace QuoteAPI
 {
@@ -25,6 +27,8 @@ namespace QuoteAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RaterContext>(opt =>
+            opt.UseInMemoryDatabase("RaterList"));
             services.AddControllers();
         }
 
